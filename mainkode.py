349be14471b76f40fd1e5f91c39cc20a77d97724
anton <anton.h.troelsen@gmail.com>
2024-03-04 13:51:49 +0100

    nu burde der være rigtige enheder

--- mainkode.py
+++ mainkode.py
@@ -9,7 +9,7 @@ from Functions import ProsumerOptimizer
 from Functions import Netting
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
-
+#%%
 
 ### Import price and prosumer data ###
 df_prices, df_pro = LoadData()
@@ -68,54 +68,54 @@ axs[0, 0].bar(hourly['Hour'], hourly['SpotPriceDKK'])
 axs[0, 0].set_xlabel('Hour')
 axs[0, 0].set_ylabel('Average Spot Price (DKK/MWh)')
 axs[0, 0].set_title('Hourly Average Spot Price 2019-2023')
-axs[0, 0].set_ylim([0, max(hourly2022['SpotPriceDKK'])])
+axs[0, 0].set_ylim([0, max(hourly2022['SpotPriceDKK'])+100])
 
 axs[0, 1].bar(hourly2019['Hour'], hourly2019['SpotPriceDKK'])
 axs[0, 1].set_xlabel('Hour')
 axs[0, 1].set_ylabel('Average Spot Price (DKK/MWh)')
 axs[0, 1].set_title('Hourly Average Spot Price 2019')
-axs[0, 1].set_ylim([0, max(hourly2022['SpotPriceDKK'])])
+axs[0, 1].set_ylim([0, max(hourly2022['SpotPriceDKK'])+100])
 
 axs[0, 2].bar(hourly2020['Hour'], hourly2020['SpotPriceDKK'])
 axs[0, 2].set_xlabel('Hour')
 axs[0, 2].set_ylabel('Average Spot Price (DKK/MWh)')
 axs[0, 2].set_title('Hourly Average Spot Price 2020')
-axs[0, 2].set_ylim([0, max(hourly2022['SpotPriceDKK'])])
+axs[0, 2].set_ylim([0, max(hourly2022['SpotPriceDKK'])+100])
 
 axs[1, 0].bar(hourly2021['Hour'], hourly2021['SpotPriceDKK'])
 axs[1, 0].set_xlabel('Hour')
 axs[1, 0].set_ylabel('Average Spot Price (DKK/MWh)')
 axs[1, 0].set_title('Hourly Average Spot Price 2021')
-axs[1, 0].set_ylim([0, max(hourly2022['SpotPriceDKK'])])
+axs[1, 0].set_ylim([0, max(hourly2022['SpotPriceDKK'])+100])
 
 axs[1, 1].bar(hourly2022['Hour'], hourly2022['SpotPriceDKK'])
 axs[1, 1].set_xlabel('Hour')
 axs[1, 1].set_ylabel('Average Spot Price (DKK/MWh)')
 axs[1, 1].set_title('Hourly Average Spot Price 2022')
-axs[1, 1].set_ylim([0, max(hourly2022['SpotPriceDKK'])])
+axs[1, 1].set_ylim([0, max(hourly2022['SpotPriceDKK'])+100])
 
 axs[1, 2].bar(hourly2023['Hour'], hourly2023['SpotPriceDKK'])
 axs[1, 2].set_xlabel('Hour')
 axs[1, 2].set_ylabel('Average Spot Price (DKK/MWh)')
 axs[1, 2].set_title('Hourly Average Spot Price 2023')
-axs[1, 2].set_ylim([0, max(hourly2022['SpotPriceDKK'])])
+axs[1, 2].set_ylim([0, max(hourly2022['SpotPriceDKK'])+100])
 
 
 plt.tight_layout()
 plt.show()
 
 
-daily_avg_prices = df_prices.groupby(df_prices["HourDK"].dt.date)["SpotPriceDKK"].mean().reset_index()
+daily_avg_prices = df_prices.groupby(df_prices["HourDK"].dt.date)["Buy"].mean().reset_index()
 
 
 ##################################### TASK 2 ##################################
-
+#%%
 #2.1
 params = {
-    'Pmax': 5,
+    'Pmax': 5/1000,
     'n_c': 0.99,
     'n_d': 0.99,
-    'Cmax': 10
+    'Cmax': 10/1000
 }
 params['C_0'] = 0.1 * params['Cmax']
 params['C_n'] = 0.5 * params['Cmax']
@@ -163,7 +163,7 @@ plt.figure()
 plt.bar(Yearly["Year"],yearprf)
 plt.xlabel("year")
 plt.ylabel("profit in DKK")
-plt.title("yerly profit from 2019 to 2023")
+plt.title("Yearly profit from 2019 to 2023")
 plt.show()
 plt.close()
 
@@ -171,25 +171,26 @@ plt.close()
 
 plt.figure()
 plt.bar(daily_avg_prices["HourDK"],daylyprofit)
-plt.bar(daily_avg_prices["HourDK"],daily_avg_prices["SpotPriceDKK"])
+plt.bar(daily_avg_prices["HourDK"],daily_avg_prices["Buy"]/1000)
 plt.xlabel("year")
-plt.ylabel("profit in DKK")
-plt.legend(["Profit", "Price"])
-plt.title("yerly profit from 2019 to 2023")
+plt.ylabel("DKK")
+plt.legend(["Profit in dkk", "Price in DKK/kWh"])
+plt.title("Dayly profit and Price from 2019 to 2023")
 plt.show()
 plt.close()
 
-
+correlation = np.corrcoef(daylyprofit, daily_avg_prices["Buy"])
+print("The correlation between the dayly profit and the price is: " + str(correlation[0,1]))
 
 
 
 #2.3 
 
 params = {
-    'Pmax': 5,
+    'Pmax': 5/1000,
     'n_c': 0.95,
     'n_d': 0.95,
-    'Cmax': 10
+    'Cmax': 10/1000
 }
 params['C_0'] = 0.1 * params['Cmax']
 params['C_n'] = 0.5 * params['Cmax']
@@ -227,10 +228,10 @@ for year in range(2019, 2024):
 
 
 params = {
-    'Pmax': 5,
+    'Pmax': 5/1000,
     'n_c': 0.90,
     'n_d': 0.90,
-    'Cmax': 10
+    'Cmax': 10/1000
 }
 params['C_0'] = 0.1 * params['Cmax']
 params['C_n'] = 0.5 * params['Cmax']
@@ -285,6 +286,11 @@ plt.show()
 
 ### task 3 ###
 
+"""
+# covert from kw to mw
+df_pro["Load"] = df_pro["Load"] / 1000
+df_pro["PV"] = df_pro["PV"] / 1000
+"""
 #3.1
 df_pro['consumer_cost'] = df_pro['Load'] * df_prices['Buy']
 
@@ -373,6 +379,13 @@ plt.show()
 #%%
 # 3.3?
 # the params for this assignamet
+
+
+#convert buy and sell to dkk/kwh
+df_prices["Buy"] = df_prices["Buy"] / 1000
+df_prices["Sell"] = df_prices["Sell"] / 1000
+
+
 params = {
     'Pmax': 5,
     'n_c': 0.95,
@@ -414,5 +427,11 @@ for year in range(2022, 2024):
     total_cost = 0
 
 
+plt.figure()  
+plt.plot(consuption_cost)
+plt.xlabel("day")
+plt.ylabel("Consumption Cost (DKK)")
+plt.title("Monthly Consumption Cost")
+plt.show()
 
 #%%
